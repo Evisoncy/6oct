590f84a053a566fe3dde01b7083ba21d3dc43a21
Cesar-arzapalo <cesar.arzapalo@unmsm.edu.pe>
2021-10-06 16:36:57 -0500

    actualizando vista hasta la seccion de login

--- src/PMTD/Iniciar.java
+++ src/PMTD/Iniciar.java
@@ -46,65 +46,86 @@ public class Iniciar extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
-        jButton2 = new javax.swing.JButton();
-        jButton1 = new javax.swing.JButton();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel1 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
+        vistaLogin = new javax.swing.JPanel();
+        encabezado = new javax.swing.JPanel();
+        logo = new javax.swing.JLabel();
+        titulo1 = new javax.swing.JLabel();
+        login = new javax.swing.JPanel();
+        ingresar = new javax.swing.JButton();
+        documentacion = new javax.swing.JButton();
+        jLabel8 = new javax.swing.JLabel();
+        jTextField2 = new javax.swing.JTextField();
+        pieDePagina = new javax.swing.JPanel();
+        titulo = new javax.swing.JLabel();
         jButton3 = new javax.swing.JButton();
         jButton4 = new javax.swing.JButton();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
-        jLabel6 = new javax.swing.JLabel();
-        jLabel7 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
+        VistaCarrusel = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
-        jPanel1.setBorder(javax.swing.BorderFactory.createEtchedBorder());
-        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        vistaLogin.setBackground(new java.awt.Color(31, 103, 92));
+        vistaLogin.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        vistaLogin.setMinimumSize(new java.awt.Dimension(320, 360));
+        vistaLogin.setVerifyInputWhenFocusTarget(false);
+        vistaLogin.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/icons8_pdf_48px.png"))); // NOI18N
-        jButton2.setText("CARGA DE ARCHIVOS");
-        jButton2.setBorder(null);
-        jButton2.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        encabezado.setOpaque(false);
+        encabezado.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        logo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/Inicio/Encabezado/escudo.png"))); // NOI18N
+        logo.setText("jLabel8");
+        encabezado.add(logo, new org.netbeans.lib.awtextra.AbsoluteConstraints(115, 20, 90, 120));
+
+        titulo1.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        titulo1.setForeground(new java.awt.Color(255, 255, 255));
+        titulo1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        titulo1.setText("PROCESO MILITAR DE TOMA DE DECISIONES");
+        encabezado.add(titulo1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 140, 320, 60));
+
+        vistaLogin.add(encabezado, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 320, 200));
+
+        login.setOpaque(false);
+        login.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        ingresar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/Inicio/Login/Botones/Ingresar.png"))); // NOI18N
+        ingresar.setContentAreaFilled(false);
+        ingresar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                ingresarActionPerformed(evt);
             }
         });
-        jPanel1.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 380, 170, 50));
+        login.add(ingresar, new org.netbeans.lib.awtextra.AbsoluteConstraints(24, 60, 272, 32));
 
-        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/icons8_enter_48px.png"))); // NOI18N
-        jButton1.setText("INGRESAR");
-        jButton1.setBorder(null);
-        jButton1.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        jButton1.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        documentacion.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/Inicio/Login/Botones/Registrar Documentación.png"))); // NOI18N
+        documentacion.setContentAreaFilled(false);
+        documentacion.setOpaque(false);
+        documentacion.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                documentacionActionPerformed(evt);
             }
         });
-        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 380, 120, 50));
+        login.add(documentacion, new org.netbeans.lib.awtextra.AbsoluteConstraints(24, 110, 272, 32));
 
-        jLabel2.setFont(new java.awt.Font("Tahoma", 1, 15)); // NOI18N
-        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel2.setText("NOMBRE DE USUARIO");
-        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 50, -1, -1));
+        jLabel8.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/Inicio/Login/Input/Usuario.png"))); // NOI18N
+        jLabel8.setText("jLabel8");
+        login.add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(24, 10, 272, 32));
 
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/etapa2/fondo1.png"))); // NOI18N
-        jLabel1.setText("DFSDFDSFSD");
-        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 40, 270, 40));
+        jTextField2.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        jTextField2.setBorder(null);
+        jTextField2.setOpaque(false);
+        login.add(jTextField2, new org.netbeans.lib.awtextra.AbsoluteConstraints(24, 10, 240, 32));
 
-        jTextField1.setAlignmentX(10.0F);
-        jTextField1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField1ActionPerformed(evt);
-            }
-        });
-        jPanel1.add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 90, 270, 30));
+        vistaLogin.add(login, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 200, 320, 160));
+
+        pieDePagina.setOpaque(false);
+        pieDePagina.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        titulo.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        titulo.setForeground(new java.awt.Color(255, 255, 255));
+        titulo.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        titulo.setText("Luchemos contra el COVID-19");
+        pieDePagina.add(titulo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 320, 100));
 
         jButton3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/sin-sonido.png"))); // NOI18N
         jButton3.setContentAreaFilled(false);
@@ -118,7 +139,7 @@ public class Iniciar extends javax.swing.JFrame {
                 jButton3ActionPerformed(evt);
             }
         });
-        jPanel1.add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 300, -1, -1));
+        pieDePagina.add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 30, 40, -1));
 
         jButton4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/simbolo-de-interfaz-de-sonido-dibujado-a-mano-de-un-altavoz.png"))); // NOI18N
         jButton4.setContentAreaFilled(false);
@@ -132,54 +153,18 @@ public class Iniciar extends javax.swing.JFrame {
                 jButton4ActionPerformed(evt);
             }
         });
-        jPanel1.add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(440, 300, -1, -1));
+        pieDePagina.add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(265, 30, 50, -1));
 
-        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/menu/giexplosion.gif"))); // NOI18N
-        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(610, 280, 190, 180));
+        vistaLogin.add(pieDePagina, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 360, 320, 100));
 
-        jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/menu/giexplosion.gif"))); // NOI18N
-        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 280, 190, 180));
+        getContentPane().add(vistaLogin, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, 460));
 
-        jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/menu/escudo2.png"))); // NOI18N
-        jPanel1.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 150, -1, -1));
-
-        jLabel7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/menu/bicentenario.png"))); // NOI18N
-        jPanel1.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(610, 160, -1, -1));
-
-        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/menu/fondo.jpeg"))); // NOI18N
-        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 850, 480));
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-        );
+        VistaCarrusel.setBackground(new java.awt.Color(46, 146, 160));
+        VistaCarrusel.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        getContentPane().add(VistaCarrusel, new org.netbeans.lib.awtextra.AbsoluteConstraints(320, 0, 660, 460));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
-        Menu me = new Menu(this);
-        this.dispose();
-        me.setVisible(true);
-        JOptionPane.showMessageDialog(this, "Bienvenido Comandante");
-    }//GEN-LAST:event_jButton1ActionPerformed
-
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
-        PDF f = new PDF(null,true);
-        f.setVisible(true);
-    }//GEN-LAST:event_jButton2ActionPerformed
-
-    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField1ActionPerformed
 
     private void jButton3MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton3MouseClicked
 
@@ -196,6 +181,18 @@ public class Iniciar extends javax.swing.JFrame {
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
         MusicaFondo.reproducirMusica();
     }//GEN-LAST:event_jButton4ActionPerformed
+
+    private void ingresarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ingresarActionPerformed
+        Menu me = new Menu(this);
+        this.dispose();
+        me.setVisible(true);
+        JOptionPane.showMessageDialog(this, "Bienvenido Comandante");
+    }//GEN-LAST:event_ingresarActionPerformed
+
+    private void documentacionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_documentacionActionPerformed
+        PDF f = new PDF(null,true);
+        f.setVisible(true);
+    }//GEN-LAST:event_documentacionActionPerformed
 
     /**
      * @param args the command line arguments
@@ -236,18 +233,19 @@ public class Iniciar extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
+    private javax.swing.JPanel VistaCarrusel;
+    private javax.swing.JButton documentacion;
+    private javax.swing.JPanel encabezado;
+    private javax.swing.JButton ingresar;
     private javax.swing.JButton jButton3;
     private javax.swing.JButton jButton4;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel7;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JTextField jTextField1;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JTextField jTextField2;
+    private javax.swing.JPanel login;
+    private javax.swing.JLabel logo;
+    private javax.swing.JPanel pieDePagina;
+    private javax.swing.JLabel titulo;
+    private javax.swing.JLabel titulo1;
+    private javax.swing.JPanel vistaLogin;
     // End of variables declaration//GEN-END:variables
 }
